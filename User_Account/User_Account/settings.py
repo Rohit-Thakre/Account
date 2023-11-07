80cf4a8490f9d1d5c742f4fe5dd7205f96e70c97
Rohit Thakre <thake785@gmail.com>
2023-11-07 22:50:29 +0530

    icon added.

--- User_Account/User_Account/settings.py
+++ User_Account/User_Account/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fhcbf*t$!fd2z7jjqkcrj$1u6n&c4*71xx2v3#jczsy5ir=$o4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
